071ac48e907587d34d64ed9d01c31105ab75e7d0
Илья Матвеев <ilamatveev@MacBook-Pro-Ila.local>
2021-03-15 16:34:05 +0300

    buttons

--- appJS/index.js
+++ appJS/index.js
@@ -10,7 +10,7 @@ let profileSubtitle = document.querySelector('.profile__subtitle');
 
 let popupSubmit = document.querySelector('.popup__button-submit');
 
-let buttonLike = document.querySelector('.places__button-like');
+let buttonLikes = document.querySelectorAll('.places__button-like');
 
 function openPopup() {
     popup.classList.remove('popup__display');
@@ -21,22 +21,43 @@ function popupClose() {
 }
 
 editButton.addEventListener('click', function (event) {
-    openPopup();
     event.preventDefault();
-} );
+    openPopup();
+});
 
 closePopup.addEventListener('click', function (event) {
-    popupClose()
     event.preventDefault();
-})
+    popupClose()
+});
 
 popupSubmit.addEventListener('click', function (event) {
+    event.preventDefault();
     let fistName = document.querySelector('.popup__firstname').value;
     let popupParagraph = document.querySelector('.popup__paragraph').value;
     profileTitle.textContent = fistName;
     profileSubtitle.textContent = popupParagraph;
     popupClose();
-    event.preventDefault();
-})
+});
 
+const cardLikes = Array(6).fill(false);
 
+/*
+for ( let i = 0; i < buttonLikes.length ; i++ ) {
+    let likeActive = buttonLikes[i];
+    likeActive.addEventListener('click', function (event) {
+        event.preventDefault();
+        cardLikes[i] = !cardLikes[i];
+        const img = likeActive.querySelector('.places__button-image');
+        img.src = cardLikes[i] ? 'images/like-button-active.svg' : 'images/like-button.svg';
+    });
+}
+*/
+
+buttonLikes.forEach((buttonLike, i) =>
+    buttonLike.addEventListener('click', e => {
+        e.preventDefault();
+        cardLikes[i] = !cardLikes[i];
+        const img = buttonLike.querySelector('.places__button-image');
+        img.src = cardLikes[i] ? 'images/like-button-active.svg' : 'images/like-button.svg';
+    }),
+);
