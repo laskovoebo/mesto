55b2c67faedbcf383f496da873a11f6e9b21523f
Илья Матвеев <ilamatveev@MacBook-Pro-Ila.local>
2021-03-30 19:40:00 +0300

    java

--- appJS/index.js
+++ appJS/index.js
@@ -1,8 +1,10 @@
 let editButton = document.querySelector('.profile__edit-button');
 
-let popup = document.querySelector('.popup');
+let popupProfile = document.querySelector('#profilePopup');
+const popupCards = document.querySelector('#cardsPopup');
 
-let buttonClosePopup = document.querySelector('.popup__button-close');
+let buttonCloseProfile = document.querySelector('#closeProfile');
+let buttonCloseCards = document.querySelector('#closeCards');
 
 let profileTitle = document.querySelector('.profile__title');
 
@@ -12,30 +14,49 @@ let popupSubmit = document.querySelector('.popup__button-submit');
 
 let buttonLikes = document.querySelectorAll('.places__button-like');
 
-let firstName = document.querySelector('.popup__input-firstname');
+let firstName = document.querySelector('.popup__input_text_firstname');
 
-let popupParagraph = document.querySelector('.popup__input-paragraph');
+let popupParagraph = document.querySelector('.popup__input_text_paragraph');
 
-function openPopup () {
-    popup.classList.remove('popup_display');
-    firstName.value =  profileTitle.textContent;
+const openPopupCards = document.querySelector('.profile__submit-button');
+
+function openProfile () {
+    popupProfile.classList.add('popup_display');
+    firstName.value = profileTitle.textContent;
     popupParagraph.value = profileSubtitle.textContent;
 }
 
-function closePopup() {
-    popup.classList.add('popup_display');
+function openCards() {
+    console.log(popupCards.classList);
+    popupCards.classList.add('popup__display');
+    console.log(popupCards.classList);
+}
+
+function closeProfile() {
+    popupProfile.classList.remove('popup_display');
+}
+
+function closeCards() {
+    popupCards.classList.remove('popup_display');
 }
 
 function popupSubmitHandler (event) {
     event.preventDefault();
     profileTitle.textContent = firstName.value;
     profileSubtitle.textContent = popupParagraph.value;
-    closePopup();
+    closeProfile();
 }
 
-editButton.addEventListener('click', openPopup);
 
-buttonClosePopup.addEventListener('click', closePopup);
+editButton.addEventListener('click', openProfile);
+openPopupCards.addEventListener('click', openCards);
 
-document.formPopup.addEventListener('submit', popupSubmitHandler);
+buttonCloseProfile.addEventListener('click', closeProfile);
+buttonCloseCards.addEventListener('click', closeCards);
+
+
+// document.formPopup.addEventListener('submit', popupSubmitHandler);
+
+
+
 
